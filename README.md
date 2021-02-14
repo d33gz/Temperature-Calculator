07b741dadb1398a4d2a832da69f19ecdaefdafb4
Steven Laptop <sdhdh2@gmail.com>
2021-02-14 11:05:32 -0600

    initial

---
+++ README.md
@@ -0,0 +1,13 @@
+Currently we have some basic JavaScript that can convert temperature from Kelvin into Celsius and then Fahrenheit.
+
+The next step is convert the code into a function which takes in an integer as Kelvin that will return Fahrenheit.
+
+Then make functions to process Kelvin into Celsius, Celsius into Kelvin, Celsius into Fahrenheit, Fahrenheit into Kelvin, and Fahrenheit into Celsius.
+
+With that finished, we'll need to create an HTML form for putting in the numbers and a drop down box for choosing what factors to convert to and from.
+
+Last, a couple of simple CSS stylings.
+
+And that will complete this app! A fully functioning Temperature Calculator to use whenever I need it :)
+
+BONUS GOAL add in Newton Scale Functionality
---
+++ index.js
@@ -0,0 +1,34 @@
+//Several example Kelvin to Fahrenheit Calculations
+
+  //Today's forecast in Kelvin at 293
+  const kelvin = 293;
+  //Converting Kelvin to Celsius
+  let celsius = kelvin - 273;
+  //Converting Celsius to Fahrenheit
+  let fahrenheit = celsius * (9/5) + 32;
+  //Rounding down Fahrenheit to a Whole Number
+  fahrenheit = Math.floor(fahrenheit);
+  //The Console Log
+  console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`);
+
+  //Today's forecast in Kelvin at 0
+  const kelvin = 0;
+  //Converting Kelvin to Celsius
+  let celsius = kelvin - 273;
+  //Converting Celsius to Fahrenheit
+  let fahrenheit = celsius * (9/5) + 32;
+  //Rounding down Fahrenheit to a Whole Number
+  fahrenheit = Math.floor(fahrenheit);
+  //The Console Log
+  console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`);
+
+//One example Celsius to Newton Calculation
+
+  //Today's forecast in Celsius
+  const celsius = 50;
+  //Converting Celsius to Newton
+  let Newton = celsius * (33/100);
+  //Rounding down Newton to a Whole Number
+  Newton = Math.floor(Newton);
+  //The Console Log
+  console.log(`The temperature is ${Newton} degrees Newton`);
