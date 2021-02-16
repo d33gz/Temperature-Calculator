daca725d9e7ffa38115b80e515b5f19518a04f30
Steven Laptop <sdhdh2@gmail.com>
2021-02-16 14:58:47 -0600

    kelvin to fahrenheit

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 Currently we have some basic JavaScript that can convert temperature from Kelvin into Celsius and then Fahrenheit.
 
-The next step is convert the code into a function which takes in an integer as Kelvin that will return Fahrenheit.
-
 Then make functions to process Kelvin into Celsius, Celsius into Kelvin, Celsius into Fahrenheit, Fahrenheit into Kelvin, and Fahrenheit into Celsius.
 
 With that finished, we'll need to create an HTML form for putting in the numbers and a drop down box for choosing what factors to convert to and from.
--- index.js
+++ index.js
@@ -11,24 +11,35 @@
   //The Console Log
   console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`);
 
-  //Today's forecast in Kelvin at 0
-  const kelvin = 0;
-  //Converting Kelvin to Celsius
-  let celsius = kelvin - 273;
-  //Converting Celsius to Fahrenheit
-  let fahrenheit = celsius * (9/5) + 32;
-  //Rounding down Fahrenheit to a Whole Number
-  fahrenheit = Math.floor(fahrenheit);
-  //The Console Log
-  console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`);
+  // //Today's forecast in Kelvin at 0
+  // const kelvin = 0;
+  // //Converting Kelvin to Celsius
+  // let celsius = kelvin - 273;
+  // //Converting Celsius to Fahrenheit
+  // let fahrenheit = celsius * (9/5) + 32;
+  // //Rounding down Fahrenheit to a Whole Number
+  // fahrenheit = Math.floor(fahrenheit);
+  // //The Console Log
+  // console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`);
 
 //One example Celsius to Newton Calculation
 
-  //Today's forecast in Celsius
-  const celsius = 50;
-  //Converting Celsius to Newton
-  let Newton = celsius * (33/100);
-  //Rounding down Newton to a Whole Number
-  Newton = Math.floor(Newton);
-  //The Console Log
-  console.log(`The temperature is ${Newton} degrees Newton`);
+  // //Today's forecast in Celsius
+  // const celsius = 50;
+  // //Converting Celsius to Newton
+  // let Newton = celsius * (33/100);
+  // //Rounding down Newton to a Whole Number
+  // Newton = Math.floor(Newton);
+  // //The Console Log
+  // console.log(`The temperature is ${Newton} degrees Newton`);
+
+//The Functions
+//Kelvin to Fahrenheit
+  function kelvinToFahrenheit(temperature) {
+    let celsius = temperature - 273;
+    let fahrenheit = celsius * (9/5) + 32;
+    fahrenheit = Math.floor(fahrenheit);
+    console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`);
+  };
+//Create Master Function that takes one parameter, (temperature)
+//This function will use /if else/ statements to determine which other function is the best to use and pass (temperature) down to them
