c78162bfa2f47cb405872ffbd53d6b3622262571
Steven Laptop <sdhdh2@gmail.com>
2021-02-16 17:23:22 -0600

    calculating functions

--- index.js
+++ index.js
@@ -1,27 +1,3 @@
-//Several example Kelvin to Fahrenheit Calculations
-
-  //Today's forecast in Kelvin at 293
-  const kelvin = 293;
-  //Converting Kelvin to Celsius
-  let celsius = kelvin - 273;
-  //Converting Celsius to Fahrenheit
-  let fahrenheit = celsius * (9/5) + 32;
-  //Rounding down Fahrenheit to a Whole Number
-  fahrenheit = Math.floor(fahrenheit);
-  //The Console Log
-  console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`);
-
-  // //Today's forecast in Kelvin at 0
-  // const kelvin = 0;
-  // //Converting Kelvin to Celsius
-  // let celsius = kelvin - 273;
-  // //Converting Celsius to Fahrenheit
-  // let fahrenheit = celsius * (9/5) + 32;
-  // //Rounding down Fahrenheit to a Whole Number
-  // fahrenheit = Math.floor(fahrenheit);
-  // //The Console Log
-  // console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`);
-
 //One example Celsius to Newton Calculation
 
   // //Today's forecast in Celsius
@@ -36,10 +12,50 @@
 //The Functions
 //Kelvin to Fahrenheit
   function kelvinToFahrenheit(temperature) {
-    let celsius = temperature - 273;
-    let fahrenheit = celsius * (9/5) + 32;
+    let fahrenheit = (9/5) * (temperature - 273) + 32;
     fahrenheit = Math.floor(fahrenheit);
-    console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`);
+    console.log(`The temperature is ${fahrenheit} degrees Fahrenheit. Taken from ${temperature} degrees Kelvin.`);
   };
+
+//Kelvin to Celsius
+  function kelvinToCelsius(temperature) {
+    let celsius = temperature - 273;
+    console.log(`The temperature is ${celsius} degrees Celsius. Taken from ${temperature} degrees Kelvin.`);
+  };
+
+//Celsius to Kelvin
+  function celsiusToKelvin(temperature) {
+    let kelvin = temperature + 273;
+    console.log(`The temperature is ${kelvin} degrees Kelvin. Taken from ${temperature} degrees Celsius.`);
+  };
+
+//Celsius to Fahrenheit
+  function celsiusToFahrenheit(temperature) {
+    let fahrenheit = (9/5) * temperature + 32;
+    fahrenheit = Math.floor(fahrenheit);
+    console.log(`The temperature is ${fahrenheit} degrees Fahrenheit. Taken from ${temperature} degrees Celsius.`);
+  };
+
+//Fahrenheit to Kelvin
+  function fahrenheitToKelvin(temperature) {
+    let kelvin = (5/9) * (temperature - 32) + 273;
+    kelvin = Math.floor(kelvin);
+    console.log(`The temperature is ${kelvin} degrees Kelvin. Taken from ${temperature} degrees Fahrenheit.`);
+  };
+
+//Fahrenheit to Celsius
+  function fahrenheitToCelsius(temperature) {
+    let celsius = (temperature - 32)/(9/5);
+    celsius = Math.floor(celsius);
+    console.log(`The temperature is ${celsius} degrees Celsius. Taken from ${temperature} degrees Fahrenheit.`);
+  };
+
+//The Function Calls
+  kelvinToFahrenheit(373);
+  kelvinToCelsius(273);
+  celsiusToKelvin(100);
+  celsiusToFahrenheit(0);
+  fahrenheitToKelvin(212);
+  fahrenheitToCelsius(32);
 //Create Master Function that takes one parameter, (temperature)
 //This function will use /if else/ statements to determine which other function is the best to use and pass (temperature) down to them
